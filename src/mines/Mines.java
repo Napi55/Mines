f0cdf80288882f956ca6d0d08a7380f801e360ed
Napi55 <88117366+Napi55@users.noreply.github.com>
2023-05-15 21:32:11 +0200

    Update Mines.java
    ajouter les commentaires  methode Mines

--- src/mines/Mines.java
+++ src/mines/Mines.java
@@ -15,21 +15,40 @@ public class Mines extends JFrame {
 
     private JLabel statusbar;
     
-    public Mines() {
+  /**
+ * Constructeur de la classe Mines.
+ * Initialise la fenêtre du jeu et crée un nouveau plateau de jeu.
+ * @param none
+ * @return none
+ */
+public Mines() {
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(WIDTH, HEIGHT);
-        setLocationRelativeTo(null);
-        setTitle("Minesweeper");
+    // Ferme la fenêtre lorsque l'utilisateur clique sur le bouton de fermeture
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        statusbar = new JLabel("");
-        add(statusbar, BorderLayout.SOUTH);
+    // Définit la taille de la fenêtre
+    setSize(WIDTH, HEIGHT);
 
-        add(new Board(statusbar));
+    // Centre la fenêtre sur l'écran
+    setLocationRelativeTo(null);
 
-        setResizable(false);
-        setVisible(true);
-    }
+    // Définit le titre de la fenêtre
+    setTitle("Minesweeper");
+
+    // Crée une étiquette pour afficher le statut du jeu
+    statusbar = new JLabel("");
+    add(statusbar, BorderLayout.SOUTH);
+
+    // Crée un nouveau plateau de jeu et l'ajoute à la fenêtre
+    add(new Board(statusbar));
+
+    // Empêche l'utilisateur de redimensionner la fenêtre
+    setResizable(false);
+
+    // Rend la fenêtre visible
+    setVisible(true);
+}
+
     
     public static void main(String[] args) {
         new Mines();
