6015fdc4e4010ce1573e64a2f0d4a465bed575a8
Napi55 <88117366+Napi55@users.noreply.github.com>
2023-05-15 21:26:47 +0200

    Update Mines.java

--- src/mines/Mines.java
+++ src/mines/Mines.java
@@ -5,42 +5,31 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+// Source: http://zetcode.com/tutorials/javagamestutorial/minesweeper/
 
+public class Mines extends JFrame {
+	private static final long serialVersionUID = 4772165125287256837L;
+	
+	private final int WIDTH = 250;
+    private final int HEIGHT = 290;
 
-/**
- * Constructeur de la classe Mines.
- * Initialise la fenêtre du jeu et crée un nouveau plateau de jeu.
- * @param none
- * @return none
- */
-public Mines() {
+    private JLabel statusbar;
+    
+    public Mines() {
 
-    // Ferme la fenêtre lorsque l'utilisateur clique sur le bouton de fermeture
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setTitle("Minesweeper");
 
-    // Définit la taille de la fenêtre
-    setSize(WIDTH, HEIGHT);
+        statusbar = new JLabel("");
+        add(statusbar, BorderLayout.SOUTH);
 
-    // Centre la fenêtre sur l'écran
-    setLocationRelativeTo(null);
+        add(new Board(statusbar));
 
-    // Définit le titre de la fenêtre
-    setTitle("Minesweeper");
-
-    // Crée une étiquette pour afficher le statut du jeu
-    statusbar = new JLabel("");
-    add(statusbar, BorderLayout.SOUTH);
-
-    // Crée un nouveau plateau de jeu et l'ajoute à la fenêtre
-    add(new Board(statusbar));
-
-    // Empêche l'utilisateur de redimensionner la fenêtre
-    setResizable(false);
-
-    // Rend la fenêtre visible
-    setVisible(true);
-}
-
+        setResizable(false);
+        setVisible(true);
+    }
     
     public static void main(String[] args) {
         new Mines();
